ddb69254a085d1aa30315b71b336c83c011edd91
pawelkurpinski <pkurpinski@wp.pl>
2018-03-06 10:37:46 +0100

    06.03

--- myNewTraining/script.js
+++ myNewTraining/script.js
@@ -1,19 +1,25 @@
-if(jQuery){
+if (jQuery) {
     alert("git!")
-  } else {
+} else {
     alert("coś się skiepściło!")
-  };
+};
 let rows = [0, 1, 2, 3, 4, 5, 6, 7];
-let cols = ["A","B","C","D","E","F","G","H"];
+let cols = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
 let count = 0;
 
 function draw() {
     for (let i = 0; i < rows.length; i++) {
-        for (let j = 0; j < cols.length; j++) { 
-            $( "div.chessBoard" ).append('<div class="tile"></div>');
+        for (var j = 0; j < cols.length; j++) {
+            if (i == 0 || j % 2 == 0) {
+                $("div.chessBoard").append('<div class="tileW"></div>');
+            } else {
+                for (let j = 0; j < cols.length; j++) {
+                    $("div.chessBoard").append('<div class="tile"></div>');
+                };
+            }
+            $("div.chessBoard").append('<div style="clear:both"></div>');
         };
-        $( "div.chessBoard" ).append('<div style="clear:both"></div>');
     };
 };
-draw();
+draw()
