7f3ac42b79a91383e1def49472429e31ed049777
pawelkurpinski <pkurpinski@wp.pl>
2018-04-03 10:53:11 +0200

    reverse string

--- myUdemyStephenGrider/exercises/reversestring/index.js
+++ myUdemyStephenGrider/exercises/reversestring/index.js
@@ -5,7 +5,51 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// first option
+
+function reverseOne(str) {
+    return str.split("").reverse().join(""); /* split nie zadziała bez "" a join bez nich da przecinki */
+} console.log(reverse("apple"));
+
+// second option
+
+let newArr = [];
+
+function reverse(string) {
+    const str = string.split("");
+    for (let i = (str.length -1); i > 0; i--) {
+         newArr.push(str[i]);
+    } return new newArr
+} console.log(reverse("apple"));
+
+// third option
+
+function reverse(str) {
+    let reversed = '';
+
+    for (let character of str){
+        reversed = character + reversed;
+    } return reversed;
+  } reverse("apple");
+
+module.exports = reverse;
+
+//fourth option with something new ;)
+
+function reverse(str) {
+    str.split('').reduce(() => {
+        
+    }, '');
+
+
+};
 
 module.exports = reverse;
