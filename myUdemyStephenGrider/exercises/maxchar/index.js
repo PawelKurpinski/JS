26c4b34cc6f3a43cf93211fca7c51ef4d0ca94ba
pawelkurpinski <pkurpinski@wp.pl>
2018-04-05 12:36:48 +0200

    max char

--- myUdemyStephenGrider/exercises/maxchar/index.js
+++ myUdemyStephenGrider/exercises/maxchar/index.js
@@ -8,16 +8,35 @@
 // function maxChar(str) {}
 
 
-const string = "Helo There!";
+const str = "Helo There!";
 const objectOfLetters = {};
 
-for (const i of string) {
+for (const i of str) {
     (!objectOfLetters[i]) ? objectOfLetters[i] = 1 : objectOfLetters[i]++; /* shorthand of "if" statement */
     console.log(i);  /* chars[i]=1 always but i = "H" than "e" ect */
     console.log(objectOfLetters[i]);
 } 
-function maxPeriodicity() {
-    
+
+const string = "Heeeello!";
+
+// function maxNumberOfLetters(string) {
+    const objectOfLetters = {};
+
+    for (let letter of string) {
+        objectOfLetters[letter] ? objectOfLetters++ : objectOfLetters[letter] = 1;
+    } 
+// } maxNumberOfLetters("abccccddede");
+
+
+
+let maxnumberOfOccurrences = 0;
+let maxChar = '';
+
+for (const maxIndex in objectOfLetters) {
+    if (objectOfLetters[maxIndex] > max) {
+        max = objectOfLetters [maxIndex];
+        maxLetter = maxIndex;
+    }
 }
 
 
