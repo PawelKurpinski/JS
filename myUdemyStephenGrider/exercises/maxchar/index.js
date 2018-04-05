5ca8ca7a2afcc2b6451a1021a83b55fdd0bd2d8a
pawelkurpinski <pkurpinski@wp.pl>
2018-04-05 14:58:25 +0200

    save

--- myUdemyStephenGrider/exercises/maxchar/index.js
+++ myUdemyStephenGrider/exercises/maxchar/index.js
@@ -12,36 +12,37 @@ const str = "Helo There!";
 const objectOfLetters = {};
 
 for (const i of str) {
-    (!objectOfLetters[i]) ? objectOfLetters[i] = 1 : objectOfLetters[i]++; /* shorthand of "if" statement */
-    console.log(i);  /* chars[i]=1 always but i = "H" than "e" ect */
+    (!objectOfLetters[i]) ? objectOfLetters[i] = 1: objectOfLetters[i]++; /* shorthand of "if" statement */
+    console.log(i); /* chars[i]=1 always but i = "H" than "e" ect */
     console.log(objectOfLetters[i]);
-} 
+}
 
 // -----------------------------------------------------------------------------------------------
+// here we are creating (not checking something in a finished object) an object and checking if given letter we can see for the first time or not. Thr clue is that notation: objectWithLetters[letter] === objectWithLetters["letters"] becauce "letter" is already a string but at the same time a variable witch is changing with every loop. It would not work with ["letter"] notation. 
 
 function maxNumberOfLetters(string) {
     const objectWithLetters = {};
-
     for (let letter of string) {
-        (objectWithLetters[letter]) ? objectWithLetters[letter]++ : objectWithLetters[letter] = 1;
+        (objectWithLetters[letter]) ? objectWithLetters[letter]++: objectWithLetters[letter] = 1;
+    }
+    console.log(objectWithLetters);
 
-                
-    } console.log(objectWithLetters);
-    
-    
+// ----------------------------------------
+
     let maxNumberOfOccurrences = 0;
     let maxChar = '';
-    
 
+// here below letterOfMaxOccurences is a letter and a key od the objects!
     for (const letterOfMaxOccurences in objectWithLetters) {
         
         if (objectWithLetters[letterOfMaxOccurences] > maxNumberOfOccurrences) {
             maxNumberOfOccurrences = objectWithLetters[letterOfMaxOccurences];
             maxChar = letterOfMaxOccurences;
-        } 
-    } 
+        }
+    }
     return maxChar;
-}; maxNumberOfLetters("Heeeello!");
+};
+maxNumberOfLetters("Heeeello!");
 
 
 
