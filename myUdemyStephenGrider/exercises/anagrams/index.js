8eb6bd9ae4dea514905535829d918333b5409d83
pawelkurpinski <pkurpinski@wp.pl>
2018-04-10 16:13:39 +0200

    anagrams final

--- myUdemyStephenGrider/exercises/anagrams/index.js
+++ myUdemyStephenGrider/exercises/anagrams/index.js
@@ -4,35 +4,36 @@
 // in the same quantity. Only consider characters, not spaces
 // or punctuation.  Consider capital letters to be the same as lower case
 // --- Examples
- 
-//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
-//   anagrams('Hi there', 'Bye there') --> False
+
+  // anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+  // anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
   const aCharMap = buildCharMap(stringA);
   const bCharMap = buildCharMap(stringB);
-  
+
   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
     return false;
   } /* here we are comparing keys of two objects - if they are not equal -> return false */
 
-  for (let char in aCharMap) {   
+  for (let char in aCharMap) {
     if (aCharMap[char] !== bCharMap[char]) {
       return false;
     }
-  } return true;
+  }
+  return true;
 }
 
 
 
 function buildCharMap(str) { /* this is a helper function */
-  
+
   const charMap = {};
 
-  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) { 
-  /* replace(/[^\w]/g, '') this allows us to get rid of the spaces and punctation marks. In other words - everything that is not a number or a character we want to replace with an empty string*/
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    /* replace(/[^\w]/g, '') this allows us to get rid of the spaces and punctation marks. In other words - everything that is not a number or a character we want to replace with an empty string*/
     charMap[char] = charMap[char] + 1 || 1; /* add elemenets to object charMap */
-    } 
+  }
   // console.log(charMap);                                          
   return charMap;
 }
@@ -42,4 +43,11 @@ anagrams('rail safety', 'fairy tales');
 
 // second solution
 
+function anagramsTwo(stringA, stringB) {
+  sortLetters(stringA) === sortLetters(stringB);
 
+  function sortLetters(string) {
+    return string.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+  } 
+};
+anagramsTwo('rail safety', 'fairy tales');
