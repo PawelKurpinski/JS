af99bf39acc6b0324b6b1d714cb8ed8363ba50bf
pawelkurpinski <pkurpinski@wp.pl>
2018-04-10 15:20:26 +0200

    anagrams solution

--- myUdemyStephenGrider/exercises/anagrams/index.js
+++ myUdemyStephenGrider/exercises/anagrams/index.js
@@ -30,7 +30,7 @@ function buildCharMap(str) { /* this is a helper function */
   const charMap = {};
 
   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) { 
-  /* replace(/[^\w]/g, '') this allow us to get rid of spaces and punctation marks  */
+  /* replace(/[^\w]/g, '') this allows us to get rid of the spaces and punctation marks. In other words - everything that is not a number or a character we want to replace with an empty string*/
     charMap[char] = charMap[char] + 1 || 1; /* add elemenets to object charMap */
     } 
   // console.log(charMap);                                          
@@ -38,3 +38,8 @@ function buildCharMap(str) { /* this is a helper function */
 }
 
 anagrams('rail safety', 'fairy tales');
+
+
+// second solution
+
+
