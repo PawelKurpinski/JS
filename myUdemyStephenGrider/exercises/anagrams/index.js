5909c67e3898591778c737148b7256ca7519258d
pawelkurpinski <pkurpinski@wp.pl>
2018-04-10 12:06:03 +0200

    anagrams final

--- myUdemyStephenGrider/exercises/anagrams/index.js
+++ myUdemyStephenGrider/exercises/anagrams/index.js
@@ -9,14 +9,14 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  const aCharMap = buildChar
-  Map(stringA);
+  const aCharMap = buildCharMap(stringA);
   const bCharMap = buildCharMap(stringB);
-
+  
   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
     return false;
-  }
+  } /* here we are comparing keys of two objects - if they are not equal -> return false */
 
+  
   for (let char in aCharMap) {   
     if (aCharMap[char] !== bCharMap[char]) {
       return false;
@@ -24,14 +24,17 @@ function anagrams(stringA, stringB) {
   } return true;
 }
 
+
+
 function buildCharMap(str) { /* this is a helper function */
+  
   const charMap = {};
 
   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) { 
   /* replace(/[^\w]/g, '') this allow us to get rid of spaces and punctation marks */
-    charMap[char] = charMap[char] + 1 || 1;
+    charMap[char] = charMap[char] + 1 || 1; /* add elemenets to object charMap */
     } 
-  // console.log(charMap);                                            
+  // console.log(charMap);                                          
   return charMap;
 }
 
