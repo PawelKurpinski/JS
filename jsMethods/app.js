d6f8553efd8d7869fb2aeec749896c193778da41
pawelkurpinski <pkurpinski@wp.pl>
2018-04-16 14:08:24 +0200

    js basics

--- jsMethods/app.js
+++ jsMethods/app.js
@@ -1,8 +1,21 @@
-var obj = {
-    name: "Chuck",
-    age: 45,
-    friends: ["Bob", "John", "Jack"],
-    add: function(x,y){
-        return x + y;
-    }
-}
+function inc() {
+    header.style.fontSize = '20px'; 
+};
+
+function dec() {
+    header.style.fontSize = '10px'; 
+};
+var buttonPlus = document.getElementById("plus");
+buttonPlus.onclick = inc;
+var buttonMinus = document.getElementById("minus");
+buttonMinus.onclick = dec;
+
+var header = document.getElementById("test");
+
+
+// function zamienKolor() {
+//     this.style.color = "red";
+// }
+// var test = document.getElementById("test");
+
+// test.onmouseover = zamienKolor;
