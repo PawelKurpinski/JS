26e6b1646002a63d9dd6529bd3822b53fe4e722a
pawelkurpinski <pkurpinski@wp.pl>
2018-04-20 09:45:51 +0200

    save

--- myUdemyStephenGrider/exercises/vowels/index.js
+++ myUdemyStephenGrider/exercises/vowels/index.js
@@ -9,11 +9,60 @@
 
 function vowels(str) {
     var vowelCounter = 0;
-    for (const letter of str) {
-        if (letter === "a"||"e"||"i"||"o"||"u") {
+    for (const letter of str.toLowerCase()) {
+        if (letter === "a" || "e" || "i" || "o" || "u") {
             vowelCounter += 1;
         }
-    } console.log(vowelCounter);  
-} vowels("Hi There!");
+    }
+    return vowelCounter;
+    console.log(vowelCounter);
+}
+vowels("Hi There!");
 
 // module.exports = vowels;
+
+
+// second solution
+
+
+function vowels(str) {
+    let vowelCounter = 0;
+    // const checker = "aeiou";
+    const checker = ['a', 'e', 'i', 'o', 'u']
+    for (const letter of str.toLowerCase()) {
+        if (checker.includes(letter)) {
+            vowelCounter++;
+        }
+    }
+    return vowelCounter;
+    console.log(vowelCounter);
+}
+vowels("Why do you ask?");
+
+// third solution
+
+function vowels(str) {
+        const matches = str.match(/[aeiou]/gi); /* this is a regular expression;
+        "g" makes sure that we don't stop at the first match that we find and "i" = "case insensitive", 
+        Attention! match function will return an ARRAY or without any matches it will return NULL */
+        console.log(matches);
+        return matches ? matches.length : 0; 
+        /* this if means - if matches variable returns somethin = true = an array - return it's length OR return ZERO  */
+    };
+    
+vowels('Why?')
+
+
+function vowels(str) {
+    let vowelCounter = 0;
+    // const checker = "aeiou";
+    const checker = ['a', 'e', 'i', 'o', 'u']
+    for (const letter of str.toLowerCase()) {
+        if (checker.includes(letter)) {
+            
+        }
+    }
+    return vowelCounter;
+    console.log(vowelCounter);
+}
+vowels("Why do you ask?");
