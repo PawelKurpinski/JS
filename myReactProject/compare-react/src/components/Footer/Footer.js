1e89ac279ec1877b903e672b8b9411d26ee9c622
pawelkurpinski <pkurpinski@wp.pl>
2018-05-03 13:59:59 +0200

    save

---
+++ myReactProject/compare-react/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    
+    
+    return (
+      <div className="App">
+       <h1>Footer it's me ;)</h1>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ myReactProject/compare-react/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  
+    render() {
+    return (
+      <div className="App">
+        <h1>Header it's me!!</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
