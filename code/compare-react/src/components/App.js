977928721eb9c77038cb54932a8a81424f074615
pawelkurpinski <pkurpinski@wp.pl>
2018-05-08 15:44:30 +0200

    React App with Bulma

--- code/compare-react/src/components/App.js
+++ code/compare-react/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Countdown from './Countdown'
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
       <h1>Welcome to my App!</h1>    
       
+      <Countdown />
       </div>
     );
   }
---
+++ code/compare-react/src/components/Countdown.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Countdown extends Component {
+   
+    render() {
+        return (
+        <div className>
+            <section class="hero is-success is-fullheight">
+            <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                Full Height title
+                </h1>
+                <h2 class="subtitle">
+                Full Height subtitle
+                </h2>
+            </div>
+            </div>
+        </section>
+                </div>
+        );
+    };
+};
+export default Countdown
