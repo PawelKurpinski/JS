977ef019e54c8354b45d71d1bd84ce3582d07898
Paweł <pkurpinski@wp.pl>
2018-05-09 00:05:44 +0200

    save

--- myUdemyBeginnerToPro/defaultTwo.js
+++ myUdemyBeginnerToPro/defaultTwo.js
@@ -1,4 +1,5 @@
 import { students } from "./default";
+document.write(students);
 import { students, total } from "./default";
 import { summ, multiply } from "./default";
 document.write(multiply(3, 5));
