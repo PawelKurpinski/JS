40b6103a9bf69cf2883fe780fb03199d3ac06d9e
pawelkurpinski <pkurpinski@wp.pl>
2018-05-09 17:39:20 +0200

    ES6 save

--- code/compare-react/src/components/Countdown.js
+++ code/compare-react/src/components/Countdown.js
@@ -1,51 +1,21 @@
 import React, { Component } from 'react';
 
 class Countdown extends Component {
-   
-    render() {
-        return (
-        <div className>
-            <section className="hero is-info is-fullheight is-bold has-text-centered">
-                <div className="hero-body">
-                	<div className="container">
-                		<h1 className="title">
-                			New Year is Coming Up!
-                		</h1>
-										<div className = "section">
-										
-										<nav className="level">
-											<div className="level-item has-text-centered">
-												<div>
-													<p className="heading">Days</p>
-													<p className="title">3,456</p>
-												</div>
-											</div>
-											<div className="level-item has-text-centered">
-												<div>
-													<p className="heading">Hours</p>
-													<p className="title">123</p>
-												</div>
-											</div>
-											<div className="level-item has-text-centered">
-												<div>
-													<p className="heading">Minutes</p>
-													<p className="title">456K</p>
-												</div>
-											</div>
-											<div className="level-item has-text-centered">
-												<div>
-													<p className="heading">Seconds</p>
-													<p className="title">789</p>
-												</div>
-											</div>
-										</nav>
-										
-										</div>
-                	</div>
-                </div>
-              </section>
-          </div>
-        )
-    }
-}
+	render() {
+    return (
+    <section class="hero is-success is-fullheight">
+   	 <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Full Height title
+        </h1>
+        <h2 class="subtitle">
+          Full Height subtitle
+        </h2>
+      </div>
+    </div>
+  </section>
+	);
+	}
+};
 export default Countdown
