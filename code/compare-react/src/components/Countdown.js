4fe11fd27cf694974464a188f36686a731f9dac1
pawelkurpinski <pkurpinski@wp.pl>
2018-05-11 15:16:20 +0200

    save React

--- code/compare-react/src/components/Countdown.js
+++ code/compare-react/src/components/Countdown.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react';
 
 class Countdown extends Component {
+  constructor(props){
+    super(props)
+
+    this.getRemainingTime();
+
+    this.state={
+      interval: this.getRemainingTime()
+    }
+  }
+
+  getRemainingTime(){
+    let now = new Date(),
+    newYear = new Date(now.getFullYear() + 1, 0, 1) 
+    /* +1 because we will compare current date with the one in a year 
+    or do sth like this ;) 0 i January and on is the first */
+    console.log(now, newYear) 
+    
+  }
+
 	render() {
     return (
-    <section class="hero is-success is-fullheight">
-   	 <div class="hero-body">
-      <div class="container">
-        <h1 class="title">
-          Full Height title
+    <section className="hero is-success is-fullheight has-text-centered">
+   	 <div className="hero-body">
+      <div className="container">
+        <h1 className="title">
+          New Year is comming up!
         </h1>
-        <h2 class="subtitle">
-          Full Height subtitle
-        </h2>
+        <div className='section'>
+                  <nav className="level">
+            <div className="level-item has-text-centered">
+              <div>
+                <p className="heading">DAYS</p>
+                <p className="title">3,456</p>
+              </div>
+            </div>
+            <div className="level-item has-text-centered">
+              <div>
+                <p className="heading">HOURS</p>
+                <p className="title">123</p>
+              </div>
+            </div>
+            <div className="level-item has-text-centered">
+              <div>
+                <p className="heading">MINUTES</p>
+                <p className="title">456K</p>
+              </div>
+            </div>
+            <div className="level-item has-text-centered">
+              <div>
+                <p className="heading">SECONDS</p>
+                <p className="title">789</p>
+              </div>
+            </div>
+          </nav>
+        </div>
       </div>
     </div>
   </section>
