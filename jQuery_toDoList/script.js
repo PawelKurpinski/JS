93cd6294102f45f68c3b9e13aacd5cd305b71223
pawelkurpinski <pkurpinski@wp.pl>
2018-06-07 16:20:57 +0200

    save

--- jQuery_toDoList/script.js
+++ jQuery_toDoList/script.js
@@ -1,6 +1,19 @@
-$('li').on("click",function(){
-    $(this).css('color', 'gray');
-    $(this).css('text-decoration', 'line-through');
+/* $('li').on("click", function () {
+
+    if ($(this).css("color") === 'rgb(128, 128, 128)') {
+        $(this).css({
+            color: "black",
+            textDecoration: "none"
+        });
+    } else {
+        $(this).css({
+            color: "gray",
+            textDecoration: "line-through"
+        });
+    }
+});   ATTENTION! INSTEAD OF THIS WE CAN USE CSS CLASS!! */ 
+
+
+$('li').on("click", function () {
+        $(this).toggleClass('completed');
 });
-
-
--- jQuery_toDoList/style.js
+++ jQuery_toDoList/style.js
@@ -5,5 +5,9 @@ export default StyleSheet.create({
     'border': [{ 'unit': 'px', 'value': 2 }, { 'unit': 'string', 'value': 'solid' }, { 'unit': 'string', 'value': 'gray' }],
     'width': [{ 'unit': 'px', 'value': 360 }],
     'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'string', 'value': 'auto' }, { 'unit': 'px', 'value': 0 }, { 'unit': 'string', 'value': 'auto' }]
+  },
+  'completed': {
+    'color': 'gray',
+    'textDecoration': 'line-through'
   }
 });
