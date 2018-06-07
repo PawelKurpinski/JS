858a173158f14650464e8d71b46a8f06bc558afb
pawelkurpinski <pkurpinski@wp.pl>
2018-06-07 13:58:47 +0200

    jQuery ToDoList html

---
+++ jQuery_toDoList/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'body': {
+    'backgroundColor': 'azure'
+  },
+  '#container': {
+    'backgroundColor': 'gray',
+    'width': [{ 'unit': 'px', 'value': 360 }],
+    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'string', 'value': 'auto' }, { 'unit': 'px', 'value': 0 }, { 'unit': 'string', 'value': 'auto' }]
+  }
+});
