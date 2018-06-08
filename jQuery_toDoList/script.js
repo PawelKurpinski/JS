1923a173fcdb850ad05195d3c492126fcc143efe
pawelkurpinski <pkurpinski@wp.pl>
2018-06-08 12:50:00 +0200

    save

--- jQuery_toDoList/script.js
+++ jQuery_toDoList/script.js
@@ -12,15 +12,26 @@
         });
     }
 });   ATTENTION! INSTEAD OF THIS WE CAN USE CSS CLASS!! */ 
+/* $('li').on("click", function () { */
+    /* ATTENTION! we have to add the listener to existing elements not the future ones so we have this: $('ul').on("click", "li", ... */
 
-
-$('li').on("click", function () {
+$('ul').on("click", "li", function () {
         $(this).toggleClass('completed');
 });
 
-$('span').on("click", function (event){
-     $(this).parent().fadeOut(1500, ()=> {
-        $(this).parent().remove();
+/* $('span').on("click", function (event){ */
+
+$('ul').on("click", 'span', function (event){
+     $(this).parent().fadeOut(1500, function() {
+        $(this).remove();
      });
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        let toDoText = ($(this).val());
+        $(this).val(''); /* this is an empty string - we need it to clear the input after writing down new ToDos */
+        $('ul').append(`<li><span>X</span> ${toDoText}</li>`);
+    }
 });
